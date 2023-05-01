7a00bf1969e618d40c0d1541e753eb01c6d67e3b
Bbrnn <ngugi.njoki21@students.dkut.ac.ke>
2023-05-01 12:23:16 -0700

    task0

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print sizes od different types of data*
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of char:
