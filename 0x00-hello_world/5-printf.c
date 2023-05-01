0a4bbe5f3826ad8ba5a94d6a4fb2aef2bb70f185
Bbrnn <ngugi.njoki21@students.dkut.ac.ke>
2023-05-01 11:53:52 -0700

    task5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -use printf instead of puts to print a code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
